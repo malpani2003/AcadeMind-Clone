ee3576fd5b91ae9975d86dcf9e466310ae05ec9f
Pranav Malpani <98647622+malpani2003@users.noreply.github.com>
2022-10-05 13:57:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+$(".day-1").click(function () {
+    $(".day_1_class").slideToggle()
+});
+
+$(".day-2").click(function () {
+    $(".day_2_class").slideToggle()
+});
+
+$(".day-3").click(function () {
+    $(".day_3_class").slideToggle()
+});
+
+$(".day-4").click(function () {
+    $(".day_4_class").slideToggle()
+});
+
+function play_promo(){
+    video=document.getElementById("video_intro")
+    video.play()
+}
+
+
